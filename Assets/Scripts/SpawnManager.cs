87f54c2f885720ab032c6aae5ce3042f6cef2d07
Eric Philippot <ericpdev@gmail.com>
2022-07-01 00:53:54 -0500

    refactored some code in Spawn Manager

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,28 +4,23 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    [SerializeField] Enemy enemy;
-    [SerializeField] GameObject _enemyPrefab;
+    [SerializeField] GameObject _enemyPrefab, _enemyContainer;
     private bool gameOver = false;
-    // Start is called before the first frame update
+
+
     void Start()
     {
-        enemy = _enemyPrefab.GetComponent<Enemy>();
         StartCoroutine(EnemySpawnRoutine());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     IEnumerator EnemySpawnRoutine () {
+        Vector3 randomEnemyPosition = new Vector3(Random.Range(-9, 9), 8, 0);
         
-        while (!gameOver && enemy != null) 
+        while (!gameOver && _enemyPrefab != null) 
         {
-            Instantiate<GameObject>(_enemyPrefab, new Vector3(enemy.randomSpawnLocation, 8, 0), enemy.transform.rotation);
-            yield return new WaitForSeconds(5);
+            GameObject newEnemy = Instantiate<GameObject>(_enemyPrefab, randomEnemyPosition, Quaternion.identity);
+            newEnemy.transform.parent = _enemyContainer.transform;
+            yield return new WaitForSeconds(Random.Range(2.0f, 6.0f));
         }
     }
 }
